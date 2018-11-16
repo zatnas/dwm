3f0b8832896f5f1cc47a16c9d8e62d2b037dc0b9
Izzat Nasrun <izzatnasrun@gmail.com>
2018-11-16 19:47:30 +0800

    Applied alpha patch

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const char col_gray2[]         = "#444444";
 static const char col_gray3[]         = "#bbbbbb";
 static const char col_gray4[]         = "#eeeeee";
 static const char col_cyan[]          = "#005577";
-static const unsigned int baralpha    = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
+//static const unsigned int baralpha    = 0xd0;
+//static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]        = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -21,8 +21,8 @@ static const char *colors[][3]        = {
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	[SchemeNorm] = { OPAQUE, 0xd0, OPAQUE },
+	[SchemeSel]  = { OPAQUE, 0xd0, OPAQUE },
 };
 
 /* tagging */
