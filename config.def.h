c91049653c7d4acbcf20e76e07b3e245f4823992
Izzat Nasrun <izzatnasrun@gmail.com>
2018-11-20 04:26:33 +0800

    Added SHCMD (Shell command) example

--- config.def.h
+++ config.def.h
@@ -68,6 +68,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	//{ MODKEY,			XK_w,      spawn,	   SHCMD("echo \"a\\nb\" | dmenu") },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
